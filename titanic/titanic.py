e3792342c27f2808d006823a9d00516dd6a3e652
ben-cunningham <bencunningham17@gmail.com>
2017-07-12 21:31:21 -0700

    Use pandas to handle data

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -1,4 +1,5 @@
-import csv
+import pandas as pd
+import numpy as np
 
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.svm import SVC
@@ -21,52 +22,6 @@ def is_number(s):
      
     return False
 
-def get_training_data():
-    training_data = []
-    target = []
-    with open('../data/titanic/train.csv', 'r') as csvfile:
-        rows = csv.reader(csvfile)
-        features = [2, 4, 5, 6, 7, 9, 11] #index of features we want to train with
-        for i, row in enumerate(rows):
-            if i == 0:
-                continue
-            training_row = []
-            for feature in features:
-                if not is_number(row[feature]):
-                    training_row.append(abs(hash(row[feature])))
-                else:
-                    training_row.append(float(row[feature]))    
-            training_data.append(training_row)
-            target.append(row[1])
-
-    return training_data, target
-
-def get_testing_data():
-    target = []
-    testing_data = []
-    with open('../data/titanic/test.csv', 'r') as csvfile:
-        rows = csv.reader(csvfile)
-        features = [1, 3, 4, 5, 6, 8, 10] #index of features we want to train with
-        for i, row in enumerate(rows):
-            if i == 0:
-                continue
-            testing_row = []
-            for feature in features:
-                if not is_number(row[feature]):
-                    testing_row.append(abs(hash(row[feature])))
-                else:
-                    testing_row.append(float(row[feature]))
-            testing_data.append(testing_row)
-
-    with open('../data/titanic/gender_submission.csv', 'r') as csvfile:
-        rows = csv.reader(csvfile)
-        for i, row in enumerate(rows):
-            if i == 0:
-                continue
-            target.append(row[1])
-
-    return testing_data, target    
-
 def output_results(predicted):
     i = 892
     print("PassengerId,Survived")
@@ -75,14 +30,67 @@ def output_results(predicted):
         i += 1
 
 if __name__ == "__main__":
-    train_data, target = get_training_data()
-    test_data, expected = get_testing_data()
+    test_data = pd.read_csv('../data/titanic/test.csv')
+    train_data = pd.read_csv('../data/titanic/train.csv')
+
+    # drop columns we don't want to train on
+    test_data.drop('PassengerId', 1, inplace=True)
+    test_data.drop('Name', 1, inplace=True)
+    test_data.drop('Ticket', 1, inplace=True)
+    test_data.drop('Cabin', 1, inplace=True)
+
+    train_data.drop('PassengerId', 1, inplace=True)
+    train_data.drop('Name', 1, inplace=True)
+    train_data.drop('Ticket', 1, inplace=True)
+    train_data.drop('Cabin', 1, inplace=True)
+
+    # convert numbers to floats/ints
+    test_data['Pclass'] = test_data['Pclass'].astype(int)
     
+    average_age = test_data['Age'].mean()
+    test_data['Age'][np.isnan(test_data['Age'])] = average_age
+    test_data['Age'] = test_data['Age'].astype(int)
+    test_data['SibSp'] = test_data['SibSp'].astype(int)
+    test_data['Parch'] = test_data['Parch'].astype(int)
+    
+    avg_fare = test_data['Fare'].mean()
+    test_data['Fare'][np.isnan(test_data['Fare'])] = avg_fare
+    test_data['Fare'] = test_data['Fare'].astype(float)
+
+    sex_dummies = pd.get_dummies(test_data['Sex'])
+    test_data.drop('Sex', 1, inplace=True)
+    test_data.join(sex_dummies)
+
+    em_dummies = pd.get_dummies(test_data['Embarked'])
+    test_data.drop('Embarked', 1, inplace=True)
+    test_data.join(em_dummies)
+
+    train_data['Pclass'] = train_data['Pclass'].astype(int)
+    
+    average_age = train_data['Age'].mean()
+    train_data['Age'][np.isnan(train_data['Age'])] = average_age
+    train_data['Age'] = train_data['Age'].astype(int)
+    train_data['SibSp'] = train_data['SibSp'].astype(int)
+    train_data['Parch'] = train_data['Parch'].astype(int)
+    train_data['Fare'] = train_data['Fare'].astype(float)
+
+    sex_dummies = pd.get_dummies(train_data['Sex'])
+    train_data.drop('Sex', 1, inplace=True)
+    train_data.join(sex_dummies)
+
+    em_dummies = pd.get_dummies(train_data['Embarked'])
+    train_data.drop('Embarked', 1, inplace=True)
+    train_data.join(em_dummies)
+    
+    # get our target data
+    target = train_data['Survived'].astype(int)
+    train_data.drop('Survived', 1, inplace=True)
+
     model =  RandomForestClassifier(n_estimators=100)
     clf = model.fit(train_data, target)
-    score = clf.score(test_data, expected)
-    print (score)
+    # score = clf.score(test_data, expected)
+    # print (score)
 
-    # predicted = clf.predict(test_data)
-    # output_results(predicted)
+    predicted = clf.predict(test_data)
+    output_results(predicted)
 
