d5767d66641f12888f979da35301d05860f19892
ben-cunningham <bencunningham17@gmail.com>
2017-07-19 21:58:06 -0700

    Add basic imports and setup program

--- airbnb-new-user/airbnb.py
+++ airbnb-new-user/airbnb.py
@@ -1,2 +1,11 @@
-if __name__ == '__main__':
+import pandas as pd
+import numpy as np
 
+from sklearn.model_selection import cross_val_score
+
+def handle_dates(df):
+    pass
+
+if __name__ == '__main__':
+    test_df = pd.read_csv('../data/airbnb-new-user/train_users.csv')
+    handle_dates(test_df)
