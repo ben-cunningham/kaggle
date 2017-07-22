6e80dd46420bb1a3dae47b15f47cf77482fc2cd2
ben-cunningham <bencunningham17@gmail.com>
2017-07-21 21:30:52 -0700

    Convert dates to seasons and delta from booking to account creation

--- airbnb-new-user/airbnb.py
+++ airbnb-new-user/airbnb.py
@@ -1,11 +1,39 @@
+from dateutil.parser import parse
+
 import pandas as pd
 import numpy as np
 
 from sklearn.model_selection import cross_val_score
 
+def as_month(mo):
+    if mo <= 3:
+        return 1
+    elif mo > 3 and mo <= 6:
+        return 2
+    elif mo > 6 and mo <= 9:
+        return 3
+    else:
+        return 4
+
 def handle_dates(df):
-    pass
+    days_to_book = []
+    season = []
+    for index, row in df.iterrows():
+        if pd.isnull(row['date_first_booking']):
+            days_to_book.append(-1)
+            season.append(-1)
+        else:
+            days = parse(row['date_first_booking']) - parse(row['date_account_created'])
+            days_to_book.append(abs(days.days))
+            date = parse(row['date_first_booking'])
+            season.append(as_month(date.month))
+    df['days_to_book'] = days_to_book
+    df['season'] = season
+    
+    df.drop('date_first_booking', 1, inplace=True)
+    df.drop('date_account_created', 1, inplace=True)
 
 if __name__ == '__main__':
     test_df = pd.read_csv('../data/airbnb-new-user/train_users.csv')
     handle_dates(test_df)
+    print test_df
